388c33c7260b1a97eb5878d71569b3238b35502f
Nicolas Noé <nicolas.noe@inbo.be>
2020-02-12 15:31:30 +0100

    New, empty main component

---
+++ src/components/RiskMap.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Toto {{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'RiskMap',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+</style>
