b6e670af26e71bee2d78c3ab3d1f6c07fd563b38
Nicolas Noé <nicolas.noe@inbo.be>
2020-02-12 15:52:57 +0100

    Basic leaflet map

--- src/components/RiskMap.vue
+++ src/components/RiskMap.vue
@@ -1,17 +1,44 @@
 <template>
-    <h1>Toto {{ msg }}</h1>
+  <div>
+    <div id="map" />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
+import L from "leaflet";
+
+type NullableMap = L.Map | null;
 
 export default Vue.extend({
-  name: 'RiskMap',
-  props: {
-    msg: String,
+  name: "RiskMap",
+  props: {},
+  data: function() {
+    return {
+      map: null as NullableMap
+    };
   },
+  mounted: function() {
+    this.initMap();
+  },
+  methods: {
+    initMap: function() {
+      this.map = L.map("map").setView([38.63, -90.23], 12);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(this.map);
+    }
+  }
 });
 </script>
 
 <style scoped>
+@import "../../node_modules/leaflet/dist/leaflet.css";
+
+div#map {
+  width: 640px;
+  height: 480px;
+}
 </style>
